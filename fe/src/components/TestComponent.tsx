260b0552fa5ec3f102d06735102456b6b366e788
vovantri123 <vovantri204@gmail.com>
2025-10-25 11:55:35 +0700

    Test: add badly formatted component

---
+++ fe/src/components/TestComponent.tsx
@@ -0,0 +1,9 @@
+export default function TestComponent() {
+const badlyFormatted={foo:"bar",baz:123}
+  return (
+    <div className="p-4 bg-blue-500 text-white rounded-lg shadow-md">
+          <h1>Test Component</h1>
+      <p>This file has bad formatting</p>
+    </div>
+  );
+}
